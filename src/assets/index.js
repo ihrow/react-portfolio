5fb7cf45e6ca0f80c72fcd4f80a017a0f6afc7d1
igor <igornotfound@gmail.com>
2023-03-22 16:39:22 +0100

    tech component

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,12 +26,16 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import none from "./company/R.png"
+import shareme from './shareme.jpg'
+import tictactoe from './tictactoe.png'
+
+import preview from './preview.png'
 
 export {
+  preview,
+  shareme,
+  tictactoe,
   none,
   logo,
   backend,
@@ -58,7 +62,4 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
-  jobit,
-  tripguide,
 };
